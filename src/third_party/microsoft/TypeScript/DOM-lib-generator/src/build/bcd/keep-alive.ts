23bc25f9f40620a3a0dee3be67a56397f3252d45
Frank Lemanschik <frank@lemanschik.com>
2023-04-15 10:28:11 +0200

    git subrepo clone https://github.com/microsoft/TypeScript-DOM-lib-generator src/third_party/microsoft/TypeScript/DOM-lib-generator
    subrepo:
      subdir:   "src/third_party/microsoft/TypeScript/DOM-lib-generator"
      merged:   "c36e436411"
    upstream:
      origin:   "https://github.com/microsoft/TypeScript-DOM-lib-generator"
      branch:   "main"
      commit:   "c36e436411"
    git-subrepo:
      version:  "0a31831"
      origin:   "git@github.com:stealify/git-subrepo.git"
      commit:   "0a31831"

---
+++ src/third_party/microsoft/TypeScript/DOM-lib-generator/src/build/bcd/keep-alive.ts
@@ -0,0 +1,63 @@
+export const forceKeepAlive: Record<string, string[]> = {
+  // Things that are incorrectly reported as unsupported.
+  // These should be filed to https://github.com/mdn/browser-compat-data/issues
+  CSSStyleDeclaration: [
+    "alignmentBaseline",
+    "baselineShift",
+    "clipRule",
+    "colorInterpolation",
+    "colorInterpolationFilters",
+    "dominantBaseline",
+    "fill",
+    "fillOpacity",
+    "fillRule",
+    "floodColor",
+    "floodOpacity",
+    "lightingColor",
+    "marker",
+    "markerEnd",
+    "markerMid",
+    "markerStart",
+    "shapeRendering",
+    "stopColor",
+    "stopOpacity",
+    "stroke",
+    "strokeDasharray",
+    "strokeDashoffset",
+    "strokeLinecap",
+    "strokeLinejoin",
+    "strokeMiterlimit",
+    "strokeOpacity",
+    "strokeWidth",
+    "textAnchor",
+  ],
+  Gamepad: ["hapticActuators"],
+  GlobalEventHandlers: [
+    "onwebkitanimationend",
+    "onwebkitanimationiteration",
+    "onwebkitanimationstart",
+  ],
+  IDBDatabase: [
+    // BCD unexpectedly is removing valid event data
+    // https://github.com/mdn/browser-compat-data/issues/15345
+    "onabort",
+    "onerror",
+  ],
+  ShadowRoot: [
+    // BCD unexpectedly is removing valid event data
+    // https://github.com/mdn/browser-compat-data/issues/15345
+    "onslotchange",
+  ],
+  WorkerGlobalScope: ["onrejectionhandled", "onunhandledrejection"],
+  XMLHttpRequestEventTarget: [
+    // BCD unexpectedly is removing valid event data
+    // https://github.com/mdn/browser-compat-data/issues/15345
+    "onabort",
+    "onerror",
+    "onload",
+    "onloadend",
+    "onloadstart",
+    "onprogress",
+    "ontimeout",
+  ],
+};
